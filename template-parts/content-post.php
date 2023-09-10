7193e010efc27985d35ae31bbb45c61ed62bb18e
Gianluca <gianlucarbone96@gmail.com>
2023-09-10 13:03:15 +0200

    update: styling breadcumb

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -19,24 +19,24 @@
     </div>
     <div class="post-breadcumb__box">
         <?php
-        the_category(' &gt; ');
-        ?>
-    </div>
-    <div class="post-breadcumb__box">
-        <?php
         $args = array(
             'orderby' => 'term_id',
             'fields' => 'ids'
         );
         $post_categories = wp_get_post_categories(get_the_ID(), $args);
-
-        $args = array(
-            'include' => $post_categories,
-            'style' => 'list',
-            'title_li' => ''
-        );
-        wp_list_categories($args);
-        //var_dump($post_categories);
         ?>
+        <ul class="post-breadcumb__box-list list-none">
+            <?php
+            $args = array(
+                'include' => $post_categories,
+                'style' => 'list',
+                'orderby' => 'term_id',
+                'hierarchical' => false,
+                'title_li' => ''
+            );
+            wp_list_categories($args);
+            //var_dump($post_categories);
+            ?>
+        </ul>
     </div>
 </article><!-- #post-<?php the_ID(); ?> -->
